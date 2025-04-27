4854470e4905329efe7afdc974c8321a95118b59
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-04-27 19:34:30 +0200

    Add project files.

---
+++ ST10382076_API_EF_PROGPOE2/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ST10382076_API_EF_PROGPOE2
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
